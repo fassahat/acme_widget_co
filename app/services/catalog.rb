f8f454f93c1fc623bbec3a84bf72ec7e40a11173
fassahat <idfassahata@gmail.com>
2025-10-01 20:25:46 +0200

    feat: add product catalog

---
+++ app/services/catalog.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Catalog
+  PRODUCTS = {
+    'R01' => { name: 'Red Widget', price: 32.95 },
+    'G01' => { name: 'Green Widget', price: 24.95 },
+    'B01' => { name: 'Blue Widget', price: 7.95 }
+  }.freeze
+end
